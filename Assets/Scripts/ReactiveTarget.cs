83674ef0d6492da4d727ae03af6114476135f7c3
jessmorrison <jessmorrison90@gmail.com>
2018-08-12 16:27:49 -0500

    08-12-18 4:20pm

--- Assets/Scripts/ReactiveTarget.cs
+++ Assets/Scripts/ReactiveTarget.cs
@@ -6,11 +6,16 @@ public class ReactiveTarget : MonoBehaviour {
 	
 	public void ReactToHit()
     {
+        WanderingAI behavior = GetComponent<WanderingAI>();
+        if (behavior != null)
+        {
+            behavior.SetAlive(false);
+        }
         StartCoroutine(Die());
     }
     private IEnumerator Die()
     {
-        this.transform.Rotate(-75*Time.deltaTime, 0, 0);
+        this.transform.Rotate(-75, 0, 0);
         yield return new WaitForSeconds(1.5f);
 
         Destroy(this.gameObject);
---
+++ Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WanderingAI : MonoBehaviour {
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+
+    public float speed = 33.0f;
+    public float obstacleRange = 11.0f;
+
+    private bool _alive; //using bool because it is a true or false situation
+
+    void Start()
+    {
+        _alive = true;
+    }
+
+
+    void Update () {
+
+        if (_alive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 0.75f, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (_fireball == null)
+                    {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                } 
+                else if (hit.distance < obstacleRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                 }
+            }   
+        }
+	}
+
+    public void SetAlive (bool alive) {
+        _alive = alive;
+    }
+}
+
